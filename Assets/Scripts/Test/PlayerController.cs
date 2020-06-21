af4d9416d5453a12a5bee24cae987c32e63ebcce
HijikiYamashita <godenter792@gmail.com>
2020-06-21 16:17:08 +0900

    死実装

---
+++ Assets/Scripts/Test/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.gameObject.transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.gameObject.transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "thorn")
+        {
+            Debug.Log("棘死");
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
